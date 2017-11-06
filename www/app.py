33d9d559d98baff03a9b2480f173a93971848371
xuman-Amy <1445357489@qq.com>
2017-11-06 16:11:46 +0800

    wrote app.py

--- www/app.py
+++ www/app.py
@@ -1,12 +1,39 @@
-import scrapy
-class MyscrapySpider(scrapy.spriders.Spider):
-    name = "myscrapy"
-    allowed_domains = ["myscrapy.org"]
-    start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-         "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"       
-        ]
-    def parse(self,response):
-        filename = response.url.split("/")[-2]
-        with open(filename,'wb')as f:
-            f.write(response.body)
+
+# coding: utf-8
+
+# In[1]:
+
+
+import logging; 
+logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+from jinja2 import Environment,FileSystemLoader
+
+import orm
+from coroweb import add_routers,add_static
+
+def init_jinja2(app,**kw):
+    logging.info('init jinja2..')
+    options = dict(
+        autoescape = kw.get('autoescape', True),
+        block_start_string = kw.get('block_start_string', '{%'),
+        block_end_string = kw.get('block_end_string', '%}'),
+        variable_start_string = kw.get('variable_start_string', '{{'),
+        variable_end_string = kw.get('variable_end_string', '}}'),
+        auto_reload = kw.get('auto_reload', True)
+    )
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
