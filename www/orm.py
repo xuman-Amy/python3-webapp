b5329296c2f86e4827f723a079b89c664302da68
xuman-Amy <1445357489@qq.com>
2017-11-06 16:20:09 +0800

    wrote orm.py

--- www/orm.py
+++ www/orm.py
@@ -1,20 +1,23 @@
 
 # coding: utf-8
 
-# In[5]:
+# In[ ]:
 
 
-#j建立连接池
-import asyncio,logging
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-def log(sql,args = ()):
-    loggin.info('SQL: %s' % sql)
-    
-@asyncio.coroutine
-def create_pool(loop, **kw):
+import asyncio, logging
+
+import aiomysql
+
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
+
+async def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
-    __pool = yield from aiomysql.create_pool(
+    __pool = await aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
         user=kw['user'],
@@ -26,144 +29,141 @@ def create_pool(loop, **kw):
         minsize=kw.get('minsize', 1),
         loop=loop
     )
-    
-print('done')
 
-
-# In[8]:
-
-
-#select
-@asyncio.coroutine
-def select(sql,args,size = None):
-    log(sql,args)
+async def select(sql, args, size=None):
+    log(sql, args)
     global __pool
-    with(yield from create_pool) as conn: #从连接池取出一个连接
-        cur = yield from conn.cursor(aiomysql.DictCursor)#获取 cursor 返回字典类型数据
-        yield from cur.execute(sql.replace('?','%s'),args or ())#? 是SQL语句的占位符 用%s来代替？ 
-        if size:
-            rs = yield from cur.fetchmany(size)
-        else:
-            rs = yield from cur.fetchall()
-        yield from cur.close()
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
         logging.info('rows returned: %s' % len(rs))
         return rs
-print('done')
 
-
-# In[9]:
-
-
-#insert update delete 返回整数表示影响的行数
-@asyncio.coroutine
-def execute(sql,args):
+async def execute(sql, args, autocommit=True):
     log(sql)
-    with (yield from create_pool) as conn:
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
         try:
-            cur = yield from conn.cursor()
-            yield from cur.execute(sql.replace('?','%s'),args)
-            affected = cur.rowcount
-            yield from cur.close() 
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
         except BaseException as e:
-            raise #捕获到异常不做处理 扔给上层处理
+            if not autocommit:
+                await conn.rollback()
+            raise
         return affected
-print('done')
-
-
-# In[ ]:
-
 
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
-    return ','.join(L)
-
-
-# In[ ]:
-
+    return ', '.join(L)
 
 class Field(object):
-    def __init__(self,name,column_type,primary_key,default):
+
+    def __init__(self, name, column_type, primary_key, default):
         self.name = name
         self.column_type = column_type
         self.primary_key = primary_key
         self.default = default
-        
-    def __str__(self):
-        return  <'%s,%s:%s'> % (self.__class__.__name__,self.column_type,self.name)
 
-#StringField
-class StringField(self,name = None,primary_key = False,default = None,ddl = 'varchar(100)'):
-    super().__init__(name,ddl,primary_key,default)
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
 
 class BooleanField(Field):
-    def __init__(self,name = None,default = False):
-        super().__init__(name,'boolean',False,default)
-        
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
 class IntegerField(Field):
-    def __init__(self,name = None,primary_key = False,default = 0):
-        super().__init__(name,'bigint',primary_key,default)
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
 class FloatField(Field):
-    def __init__(self,name = None,primary_key = False,default = 0.0):
-        super().__init__(name,'real',False,default)
 
-        
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
 class TextField(Field):
-    def __init__(self,name = None,default = None):
-        super().__init__(name,'text',False,default)
-              
 
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
 
-# In[ ]:
-
-
-#将User的信息映射出来
 class ModelMetaclass(type):
-    def __new__(cls,name,bases,attrs):
-        #排除model本身
-        if name = 'Model':
-            return type.__new__(cls,name,bases,attrs)
-        tableName = attrs.get('__table__',None) or name
-        logging.info('found model: %s(table: %s)' %(name,tableName))
-        
-        #获取所有的Field和主键名
+
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
         mappings = dict()
         fields = []
         primaryKey = None
-        for k,v in attrs.items():
-            if isinstance(v,Field):
-                logging.info('found mapping: %s ==> %s' %(k,v))
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
                 mappings[k] = v
                 if v.primary_key:
-                    #找到主键
+                    # 找到主键:
                     if primaryKey:
-                        raise RuntimeError('Duplicate primary key for field : %s' % k)
+                        raise StandardError('Duplicate primary key for field: %s' % k)
                     primaryKey = k
-                    else:
-                        fields.append(k)
+                else:
+                    fields.append(k)
         if not primaryKey:
-            raise RuntimeError('primary key not Found')
+            raise StandardError('Primary key not found.')
         for k in mappings.keys():
             attrs.pop(k)
-        escaped_fields = list(map(lambda f: "%s" % f ,fields))
-        attrs['__mappings__'] = mappngs
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
         attrs['__table__'] = tableName
-        attrs['__primary_key__'] = primaryKey
-        attrs['__fields__'] = fields
-        
-        #构建默认的SQL语句
-        attrs['__select__'] = 'select '%s','%s' from '%s'' %(primaryKey,','.join(escaped_fields),tableName)
-        attrs['__insert__'] = 'insert into '%s'('%s','%s') values('%s')' %(tableName,','.join(escaped_fields),primaryKey,create_args_string(len(escaped_fileds) + 1))
-        attrs['__update__'] = 'update '%s' set '%s' where '%s' =?' % (tableName,','.join(map(lambda f: ' '%s' = ? '%(mappings.get(f).name or f),fields)),primaryKey)
-        attrs['__delete__'] = 'delete from '%s' where '%s' = ?' %(tableName,primaryKey)
-        return type.__new__(cls,name,bases,attrs)
+        attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
 
-        
+class Model(dict, metaclass=ModelMetaclass):
 
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
 
-# In[ ]:
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
 
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
 
 #定义Model metaclass即元类
 class Model(dict,metaclass = ModelMetaclass):
@@ -221,7 +221,7 @@ class Model(dict,metaclass = ModelMetaclass):
         return [cls(**r) for r in rs]
     @classmethod
     async def findNumber(cls,selectField,where = None,args = None):
-        sql = ['select %s _num_ from '%s'' % (select_Field,cls.__table__)]
+        sql = ['select %s _num_ from `%s`' % (select_Field,cls.__table__)]
         if where:
             sql.append('where')
             sql.append(where)
@@ -233,7 +233,7 @@ class Model(dict,metaclass = ModelMetaclass):
     @classmethod
     @asyncio.coroutine
     def find(cls,pk):
-        rs = yield from select('%s where '%s' = ?' % (cls.__select__,cls.__primary_key__),[pk],1)
+        rs = yield from select('%s where `%s` = ?' % (cls.__select__,cls.__primary_key__),[pk],1)
         if len(rs) == 0:
             return None
         return cls(**rs[0])
@@ -259,47 +259,5 @@ class Model(dict,metaclass = ModelMetaclass):
         rows = await execute(self.__delete__,args)
         if rows != 1:
             logging.warn('failed to remove by primary key :affected rows: %s' % rows)
-        
-
-
-# In[12]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+print('done')
 
