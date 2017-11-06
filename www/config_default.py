380a14c953b35787ae48190a6b274e4989aeffe3
xuman-Amy <1445357489@qq.com>
2017-11-06 15:15:38 +0800

    wrote configs.py

---
+++ www/config_default.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+configs = {
+    'debug' : True
+    'db' = {
+        'host' : '127.0.0.1'
+        'port' : '3306'
+        'user' : 'www'
+        'password' : 'www'
+        'db': 'awesome'
+    },
+    'session ':{
+        'secret':'Awesome'
+    }
+}
+
